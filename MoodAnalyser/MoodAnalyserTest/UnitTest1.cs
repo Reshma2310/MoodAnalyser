3525d0c1fa6e271c3661c7a2dd63797a4bfcf869
Reshma2310 <dreshma253@gmail.com>
2022-06-06 15:55:23 +0500

    [Reshma] - Refactor code to take message using Constructor and running 2 tests

--- MoodAnalyser/MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyser/MoodAnalyserTest/UnitTest1.cs
@@ -5,15 +5,15 @@ namespace MoodAnalyserTest
         [Test]
         public void GivenStringMood_AnalyseMood_ReturnSadMood()
         {
-            MoodAnalyser.MoodAnalyserClass analyse = new MoodAnalyser.MoodAnalyserClass();
-            string actualResult = analyse.Analyse("I'm in Sad mood");
+            MoodAnalyser.MoodAnalyserClass analyse = new MoodAnalyser.MoodAnalyserClass("I'm in Sad mood");
+            string actualResult = analyse.Analyse();
             Assert.Pass("Sad", actualResult);
         }
         [Test]
         public void GivenStringInput_AnalyseMood_ReturnHappy()
         {
-            MoodAnalyser.MoodAnalyserClass analyse = new MoodAnalyser.MoodAnalyserClass();
-            string actualResult = analyse.Analyse("I'm in Any mood");
+            MoodAnalyser.MoodAnalyserClass analyse = new MoodAnalyser.MoodAnalyserClass("I'm in Happy mood");
+            string actualResult = analyse.Analyse();
             Assert.Pass("Happy", actualResult);
         }
     }
