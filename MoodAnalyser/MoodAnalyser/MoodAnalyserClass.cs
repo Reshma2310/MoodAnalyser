a585f31b922dac3051a06880bb97a10f58d92755
Reshma2310 <dreshma253@gmail.com>
2022-06-06 13:23:56 +0500

    [Reshma] - Analyse and respond Happy or Sad

---
+++ MoodAnalyser/MoodAnalyser/MoodAnalyserClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class MoodAnalyserClass
+    {
+        public string Analyse(string Mood)
+        {
+            if (Mood.Contains("sad"))
+                return "Sad Mood";
+            else
+                return "Happy Mood";
+        }
+    }
+}
---
+++ MoodAnalyser/MoodAnalyser/Program.cs
@@ -0,0 +1,4 @@
+﻿MoodAnalyser.MoodAnalyserClass analyse = new MoodAnalyser.MoodAnalyserClass();
+Console.WriteLine("Are you sad or happy");
+string input = Console.ReadLine();
+Console.WriteLine(analyse.Analyse(input));
